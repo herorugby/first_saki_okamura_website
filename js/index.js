71b972965b50a7c4289322e3f9462b397fd5e9a6
herorugby <hero.rugby1015@gmail.com>
2021-06-18 16:53:28 +0900

    new loading

--- js/index.js
+++ js/index.js
@@ -1,55 +1,69 @@
 // just size fixed of index.html top visual
+// $(function() {
+//   var hSize = $(window).height();
+//   var wSize = $(window).width();
+//   $("#ownsvg, .wrap").height(hSize);
+//   $("#ownsvg, .wrap").width(wSize);
+// });
 $(function() {
   var hSize = $(window).height();
   var wSize = $(window).width();
-  $("#ownsvg, .wrap").height(hSize);
-  $("#ownsvg, .wrap").width(wSize);
+  $(".loading, .wrap").height(hSize);
+  $(".loading, .wrap").width(wSize);
 });
 
+// $(window).resize(function() {
+//   var hSize = $(window).height();
+//   var wSize = $(window).width();
+//   $("#ownsvg, .wrap").height(hSize);
+//   $("#ownsvg, .wrap").width(wSize);
+// });
 $(window).resize(function() {
   var hSize = $(window).height();
   var wSize = $(window).width();
-  $("#ownsvg, .wrap").height(hSize);
-  $("#ownsvg, .wrap").width(wSize);
+  $(".loading, .wrap").height(hSize);
+  $(".loading, .wrap").width(wSize);
 });
-
 
 // cookie
 if ($.cookie("access") == undefined) {
   $.cookie("access", "onece");
 } else {
-  $("svg").remove();
-  $(".text").remove();
+  // $("svg").remove();
+  // $(".text").remove();
+  $(".loading").remove();
   $(".wrap").css("opacity", "1");
 }
 
 
 // svg animation
 $(function() {
-  new Vivus('ownsvg',
-    {
-      type: "sync",
-      start: 'autostart',
-      duration: 220 ,
-      pathTimingFunction: Vivus.EASE_OUT
-    },
-    function(obj){
-      obj.el.classList.add('fill');
-    },
-  );
+  // new Vivus('ownsvg',
+  //   {
+  //     type: "sync",
+  //     start: 'autostart',
+  //     duration: 220 ,
+  //     pathTimingFunction: Vivus.EASE_OUT
+  //   },
+  //   function(obj){
+  //     obj.el.classList.add('fill');
+  //   },
+  // );
 
 // svg fadeout function
 $(function() {
   $(".wrap").css("display", "none");
     setTimeout(function() {
-      $("#ownsvg").fadeOut();
-    }, 6000);
+      // $("#ownsvg").fadeOut();
+      $(".loading").fadeOut();
+    }, 5000);
   });
   $(function() {
     setTimeout(function() {
-      $(".wrap").css("display", "block");
-      $(".wrap").animate({opacity: "1"}, 1000);
-    }, 5500);
+      // $(".wrap").css("display", "block");
+      // $(".wrap").animate({opacity: "1"}, 1000);
+      $(".wrap").fadeIn();
+    }, 5000);
   });
 });
 
