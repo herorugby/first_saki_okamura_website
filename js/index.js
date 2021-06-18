09970aa15df59632212ecff2eaf4e115ca88322e
herorugby <hero.rugby1015@gmail.com>
2021-06-18 21:46:24 +0900

    new

--- js/index.js
+++ js/index.js
@@ -14,76 +14,18 @@ if ($.cookie("access") == undefined) {
 }
 
 // just size fixed of index.html top visual
-// $(function() {
-//   var hSize = $(window).height();
-//   var wSize = $(window).width();
-//   $("#ownsvg, .wrap").height(hSize);
-//   $("#ownsvg, .wrap").width(wSize);
-// });
 $(function() {
   var hSize = $(window).height();
   var wSize = $(window).width();
   $(".outer, .wrap").height(hSize);
   $(".outer, .wrap").width(wSize);
 });
-
-// $(window).resize(function() {
-//   var hSize = $(window).height();
-//   var wSize = $(window).width();
-//   $("#ownsvg, .wrap").height(hSize);
-//   $("#ownsvg, .wrap").width(wSize);
-// });
 $(window).resize(function() {
   var hSize = $(window).height();
   var wSize = $(window).width();
   $(".outer, .wrap").height(hSize);
   $(".outer, .wrap").width(wSize);
 });
-
-// cookie
-// if ($.cookie("access") == undefined) {
-//   $.cookie("access", "onece");
-// } else {
-//   // $("svg").remove();
-//   // $(".text").remove();
-//   $(".outer").remove();
-//   $(".wrap").css("opacity", "1");
-// }
-
-
-
-// svg animation
-// $(function() {
-//   new Vivus('ownsvg',
-//     {
-//       type: "sync",
-//       start: 'autostart',
-//       duration: 220 ,
-//       pathTimingFunction: Vivus.EASE_OUT
-//     },
-//     function(obj){
-//       obj.el.classList.add('fill');
-//     },
-//   );
-
-// svg fadeout function
-// $(function() {
-//   $(".wrap").css("display", "none");
-//     setTimeout(function() {
-//       // $("#ownsvg").fadeOut();
-//       $(".outer").fadeOut();
-//     }, 5000);
-//   });
-//   $(function() {
-//     setTimeout(function() {
-//       // $(".wrap").css("display", "block");
-//       // $(".wrap").animate({opacity: "1"}, 1000);
-//       $(".wrap").fadeIn();
-//     }, 5000);
-//   });
-// });
-
-
 // slideShow
 $(function() {
   $(".wrap-main").each(function() {
